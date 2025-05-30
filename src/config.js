27290533dadcf03fe51719f3a47adbcebb313731
Guoguo <16666742+imguoguo@users.noreply.github.com>
2025-05-30 07:42:08 +0000

    Add HD-RK3576-PI image and update imageMap in config.js

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ export const apiUrl = 'https://api.fedoravforce.org/stats/?platform=arm';
 export const imageMap = {
   "Orion O6": "images/orion-o6.webp",
   "ROCK 5 ITX": "images/rock-5-itx.webp",
+  "HD-RK3576-PI": "images/HD-RK3576-PI.jpeg",
 };
 
 export const mdMap = {
