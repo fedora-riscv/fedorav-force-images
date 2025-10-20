bcd6a9349524a155b88e7d72cc36098a32b88214
Guoguo <16666742+imguoguo@users.noreply.github.com>
2025-10-20 07:09:37 +0000

    Add Fogwise AIRbox Q900 and Radxa Dragon Q6A images to imageMap in config.js

--- src/config.js
+++ src/config.js
@@ -6,6 +6,8 @@ export const imageMap = {
   "Orion O6": "images/orion-o6.webp",
   "ROCK 5 ITX": "images/rock-5-itx.webp",
   "HD-RK3576-PI": "images/HD-RK3576-PI.jpeg",
+  "Fogwise AIRbox Q900": "images/fogwise-airbox-q900.webp",
+  "Radxa Dragon Q6A": "images/radxa-dragon-q6a.webp",
 };
 
 export const mdMap = {
